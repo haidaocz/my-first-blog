a79b7f7123d581ba5c823d8f2a0f54c24d1ca029
michael <chenzixing2009@gmail.com>
2016-03-24 21:42:56 -0700

    modify a bug in urls.py

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -24,7 +24,7 @@ router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
 router.register(r'posts', views.PostViewSet)
-router.register(r'articles', views.PostViewSet)
+router.register(r'articles', views.ArticleViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
